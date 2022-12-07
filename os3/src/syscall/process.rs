4838c0e8df5c15501bd1baff8598709208156aa1
ssssssyy <songyixin2000@163.com>
2022-12-07 15:03:34 +0000

    pass two

--- os3/src/syscall/process.rs
+++ os3/src/syscall/process.rs
@@ -1,3 +1,5 @@
+//! Process management syscalls
+
 use crate::config::{MAX_APP_NUM, MAX_SYSCALL_NUM};
 use crate::task::{exit_current_and_run_next, suspend_current_and_run_next, 
     TaskStatus, get_cur_start_time, get_cur_syscall};
@@ -13,6 +15,7 @@ pub struct TimeVal {
 pub struct TaskInfo {
     status: TaskStatus,
     syscall_times: [u32; MAX_SYSCALL_NUM],
+    // syscall_times: Vec<u32>,
     time: usize,
 }
 
@@ -41,6 +44,7 @@ pub fn sys_get_time(ts: *mut TimeVal, _tz: usize) -> isize {
     0
 }
 
+/// YOUR JOB: Finish sys_task_info to pass testcases
 pub fn sys_task_info(ti: *mut TaskInfo) -> isize {
     let now = get_time_us();
     let start = get_cur_start_time();
